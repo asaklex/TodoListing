a54a33527a69e5ef20418f49468c38e31191785b
Abdourahamane S Abba <asaklex@gmail.com>
2018-12-16 17:18:15 -0500

    Add DAL + Entity Framework + Migrations

--- TodoListing.Api/Startup.cs
+++ TodoListing.Api/Startup.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using TodoListing.Auth;
 using TodoListing.Auth.TokenAuth;
+using TodoListing.DAL;
 
 namespace TodoListing.Api
 {
@@ -43,6 +45,10 @@ namespace TodoListing.Api
 
                 options.AuthKeys = apikeys;
             });
+
+            //add Entity Framework 
+            var connectionString = Configuration.GetConnectionString("TodoListingDbContext");
+            services.AddDbContext<TodoListingDbContext>(options => options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ TodoListing.DAL/TodoListingDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoListing.DAL.Models;
+
+namespace TodoListing.DAL
+{
+    public class TodoListingDbContext:DbContext
+    {
+        public TodoListingDbContext(DbContextOptions<TodoListingDbContext> options) : base(options) { }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
