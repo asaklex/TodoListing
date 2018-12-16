7da5b11956b169556a3b4b1403537908b6e50931
Abdourahamane S Abba <asaklex@gmail.com>
2018-12-16 17:35:09 -0500

    Add Todo API Controller

--- TodoListing.Api/Controllers/TodoController.cs
+++ TodoListing.Api/Controllers/TodoController.cs
@@ -3,42 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TodoListing.DAL;
 
 namespace TodoListing.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     [Authorize]
-    public class ValuesController : ControllerBase
+    public class TodoController : ControllerBase
     {
-        // GET api/values
+        private TodoListingDbContext _todoListingDbContext;
+        public TodoController(TodoListingDbContext todoListingDbContext)
+        {
+            _todoListingDbContext = todoListingDbContext;
+        }
+
+        // GET: api/Todo
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/Todo/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST: api/Todo
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Todo/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
